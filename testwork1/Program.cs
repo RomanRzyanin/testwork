6953dff2c2c6650aea0842b075993c70b128d58f
Roman Rzyanin <roman@rzyanin.ru>
2024-03-05 22:43:51 +0300

    testwork1:solution_of_the_problem_v.1

--- testwork1/Program.cs
+++ testwork1/Program.cs
@@ -10,10 +10,86 @@
 4. Написать программу, решающую поставленную задачу
 5. Использовать контроль версий в работе над этим небольшим проектом (не должно быть так, что всё залито одним коммитом, как минимум этапы 2, 3, и 4 должны быть расположены в разных коммитах)
 
-Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
 Примеры:
 [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 [“Russia”, “Denmark”, “Kazan”] → []
 */
+
+
+
+// /// <summary>
+// /// Метод печатает элементы архива в обратном порядке.
+// /// </summary>
+// /// <param name="array"> архив </param>
+// /// <param name="j"> индекс последнего элемента архива </param>
+// void PrintReverseArray(int[] array, int j)
+// {
+//     //Базовый случай
+//     if (j == 0)
+//     {
+//         Console.Write($"{array[j]} ]");
+//         return;
+//     }
+//     //Рекурсивный случай
+//     Console.Write($"{array[j]}, ");
+//     PrintReverseArray(array, j - 1);
+// }
+
+string[] CreatArray(int size) //Метод заполнения массива пользователем.
+{
+    string[] array = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Enter {i + 1} element of the array: "); 
+        array[i] = Console.ReadLine()!;
+    }
+    return array;
+}
+
+int LengthArray(string[] array) //Метод подсчета количества элементов массива длинна которых <= 3
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++; 
+            //Console.WriteLine(array[i]);   
+        }
+    }
+    return count;
+}
+
+string[] CreatNewArray(string[] array, int size, int count) //Метод создания нового массива из элементов массива длинна которых <= 3
+{
+    string[] arrayNew = new string[count];
+    int cnt = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            arrayNew[cnt] = array[i];
+            cnt++; 
+              
+        }
+        
+    }
+    return arrayNew;
+}
+
+Console.Clear();
+Console.Write($"Enter the size of the array, n = ");
+int size = int.Parse(Console.ReadLine()!);
+string[] matrix = CreatArray(size);
+int count = LengthArray(matrix);
+Console.WriteLine($"Massive:\t [ {string.Join(", ", matrix)} ]");
+string[] matrixLength3 = CreatNewArray(matrix, size, count);
+Console.WriteLine($"Massive:\t [ {string.Join(", ", matrixLength3)} ]");
+//Console.WriteLine($"count = {LengthArray(matrix)}");
+// Console.Write("Massive reverse: [ ");
+// PrintReverseArray(matrix, size - 1);
